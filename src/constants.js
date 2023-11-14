e090bf78c7b78e8d01ccae276589856de928fba4
Luciano Mammino <lucianomammino@gmail.com>
2023-11-14 11:47:52 +0000

    chore: spaces vs tabs and lint also root files on tests

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const DEFAULT_ALPHABET =
-	'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 export const DEFAULT_MAX_SECRET_LENGTH = 12
 export const CHUNK_SIZE = 20000
 export const EXIT_CODE_SUCCESS = 0
--- src/jwtValidator.js
+++ src/jwtValidator.js
@@ -1,69 +1,69 @@
 export const SUPPORTED_ALGORITHM = ['HS256', 'HS384', 'HS512']
 
 export function decodeHeader(token) {
-	const parts = token.split('.')
+  const parts = token.split('.')
 
-	try {
-		const decodedHeader = JSON.parse(
-			Buffer.from(parts[0], 'base64').toString('utf-8'),
-		)
-		return decodedHeader
-		// biome-ignore lint/nursery/noUselessLoneBlockStatements: we actually want to catch possible errors
-	} catch (_e) {
-		console.log('Invalid token format. Invalid header.')
-		return null
-	}
+  try {
+    const decodedHeader = JSON.parse(
+      Buffer.from(parts[0], 'base64').toString('utf-8'),
+    )
+    return decodedHeader
+    // biome-ignore lint/nursery/noUselessLoneBlockStatements: we actually want to catch possible errors
+  } catch (_e) {
+    console.log('Invalid token format. Invalid header.')
+    return null
+  }
 }
 
 export function validateToken(token) {
-	const isTokenValid =
-		validateGeneralJwtFormat(token) && validateHmacAlgorithmHeader(token)
-	const algorithm = isTokenValid ? decodeHeader(token).alg : ''
+  const isTokenValid =
+    validateGeneralJwtFormat(token) && validateHmacAlgorithmHeader(token)
+  const algorithm = isTokenValid ? decodeHeader(token).alg : ''
 
-	return { isTokenValid, algorithm }
+  return { isTokenValid, algorithm }
 }
 
 export function validateGeneralJwtFormat(token) {
-	if (token.length === 0) {
-		console.log('Missing token')
-		return false
-	}
+  if (token.length === 0) {
+    console.log('Missing token')
+    return false
+  }
 
-	const parts = token.split('.')
+  const parts = token.split('.')
 
-	if (parts.length !== 3) {
-		console.log('Invalid token format. Invalid number of parts.')
-		return false
-	}
+  if (parts.length !== 3) {
+    console.log('Invalid token format. Invalid number of parts.')
+    return false
+  }
 
-	if (!parts.every((part) => part.length > 0)) {
-		console.log('Invalid token format. Parts should not be empty.')
-		return false
-	}
+  if (!parts.every((part) => part.length > 0)) {
+    console.log('Invalid token format. Parts should not be empty.')
+    return false
+  }
 
-	return true
+  return true
 }
 
 export function validateHmacAlgorithmHeader(token) {
-	const decodedHeader = decodeHeader(token)
+  const decodedHeader = decodeHeader(token)
 
-	if (!decodedHeader) {
-		return false
-	}
+  if (!decodedHeader) {
+    return false
+  }
 
-	if (decodedHeader.typ !== 'JWT') {
-		console.log(`Unsupported Typ: ${decodedHeader.typ}`)
-		return false
-	}
+  if (decodedHeader.typ !== 'JWT') {
+    console.log(`Unsupported Typ: ${decodedHeader.typ}`)
+    return false
+  }
 
-	if (!SUPPORTED_ALGORITHM.includes(decodedHeader.alg)) {
-		console.log(
-			`Unsupported algorithm: ${
-				decodedHeader.alg
-			}. Only ${SUPPORTED_ALGORITHM.join(', ')} are supported.`,
-		)
-		return false
-	}
+  if (!SUPPORTED_ALGORITHM.includes(decodedHeader.alg)) {
+    console.log(
+      `Unsupported algorithm: ${
+        decodedHeader.alg
+      }. Only ${SUPPORTED_ALGORITHM.join(', ')} are supported.`,
+    )
+    return false
+  }
 
-	return true
+  return true
 }
